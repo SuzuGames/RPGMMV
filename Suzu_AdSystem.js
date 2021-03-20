c15ed23ccf050864a28a82b42576ad363b0f1d76
Suzu Games <suzumito7u7@gmail.com>
2021-03-20 16:55:26 +0100

    Update Suzu_AdSystem.js

--- Suzu_AdSystem.js
+++ Suzu_AdSystem.js
@@ -1,21 +1,21 @@
 /*:
  * 
- * @plugindesc Allows you to set custom ads in your game.
+ * @plugindesc When you open the plugin you will find help.
  * @author SuzuGames | https://suzugames.wordpress.com
  * @desc To edit the plugin you need to open it. There isn't params at the moment.
- * @help When you open the plugin you will find help.
- * If you don't understand something: suzumito7u7@gmail.com
+ * @help Allows you to set custom ads in your game.
+* If you don't understand something: https://forums.rpgmakerweb.com/index.php?members/suzumito7u7.148841/
  *
  * 
- * +------------------------------+
- * |            DEVLOG            |
- * +------------------------------+
- * 
- * 
- * [v1.0] @ 20-03-21
- * 
- * - Plugin release.
- * 
+ * +----------------------------------------------+
+ * |                    DEVLOG                    |
+ * +----------------------------------------------+
+ * |                                              |
+ * |                                              |
+ * |              [v1.0] : 20-03-21               |
+ * |                                              |      
+ * | - Plugin release.                            |
+ * +----------------------------------------------+
  * 
  * 
  * Made with <3 by Suzu.
