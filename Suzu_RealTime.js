f65e7d1451bb42077ca1ec4501d06075953e27dc
Suzu Games <suzumito7u7@gmail.com>
2021-07-21 16:20:11 +0200

    Create Suzu_RealTime.js

---
+++ Suzu_RealTime.js
@@ -0,0 +1,29 @@
+/*:
+ * 
+ * @plugindesc When you open the plugin you will find help.
+ * @author SuzuGames | https://suzugames.wordpress.com
+ * @desc To edit the plugin you need to open it. There isn't params at the moment.
+ * @param Seconds Variable
+ * @desc Variable for seconds
+ * @type number
+ * @help Allows you to set custom ads in your game.
+ * Write "ad();" in a script line of any event.
+ * If you don't understand something: https://forums.rpgmakerweb.com/index.php?members/suzumito7u7.148841/
+ *
+ * 
+ * +----------------------------------------------+
+ * |                    DEVLOG                    |
+ * +----------------------------------------------+
+ * |                                              |
+ * |                                              |
+ * |              [v1.0] : 20-03-21               | // CAMBIAR
+ * |                                              |      
+ * | - Plugin release.                            |
+ * +----------------------------------------------+
+ * 
+ * 
+ * Made with <3 by Suzu.
+ */ 
+
+
+var params = PluginManager.parameters("Suzu_RealTime");
